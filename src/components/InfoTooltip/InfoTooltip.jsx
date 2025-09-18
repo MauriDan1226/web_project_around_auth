991546476d508a3037476a876707c6ca9fb28156
titanram <mdrg.mauricio@outlook.com>
2025-09-17 21:36:36 -0600

    avance pt1

---
+++ src/components/InfoTooltip/InfoTooltip.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import successImage from "../../../../images/success.jpg";
+import failImage from "../../../../images/Fail.jpg";
+import "../../../../blocks/popup.css";
+import Popup from "../Popup/Popup";
+
+const InfoTooltip = ({ isOpen, onClose, isSuccess, message }) => {
+  return (
+    <Popup isOpen={isOpen} onClose={onClose} isForm={false}>
+      <div className="popup__tooltip">
+        <img
+          src={isSuccess ? successImage : failImage}
+          alt={isSuccess ? "Éxito" : "Error"}
+          className="popup__tooltip-icon"
+        />
+        <p className="popup__tooltip-text">{message}</p>
+      </div>
+    </Popup>
+  );
+};
+
+export default InfoTooltip;
---
+++ src/components/Login/login.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { api } from "../../../../utils/api";
+import InfoTooltip from "../InfoTooltip/InfoTooltip";
+import "../../../../blocks/login.css";
+
+function Login({ onLogin, tooltip, setTooltip }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(email, password);
+  };
+
+  return (
+    <>
+      <header></header>
+      <main className="login">
+        <form onSubmit={handleSubmit} className="login__container">
+          <h2 className="login__title">Iniciar Sesión</h2>
+          <input
+            className="login__input"
+            type="email"
+            placeholder="Correo Electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            className="login__input"
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit" className="login__button">
+            Iniciar Sesión
+          </button>
+        </form>
+        <p className="login__redirect">
+          ¿No eres usuario?{" "}
+          <Link to="/signup" style={{ color: "lightblue" }}>
+            Haz click aquí para registrarte
+          </Link>
+        </p>
+      </main>
+
+      <InfoTooltip
+        isOpen={tooltip.open}
+        isSuccess={tooltip.success}
+        message={tooltip.message}
+        onClose={() => setTooltip({ ...tooltip, open: false })}
+      />
+    </>
+  );
+}
+
+export default Login;
